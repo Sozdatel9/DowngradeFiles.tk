9f807b3348b553f597201db5518c0fbca3f26b3c
Sozdatel9 <sozdatel9@gmail.com>
2022-01-05 20:03:35 +0400

    Delete donate.php

--- pages/donate.php
+++
@@ -1,5 +0,0 @@
-<TR><TD COLSPAN=14><CENTER><FONT SIZE=+1 COLOR="#FCFE54"><B>Поддержите нас</B></FONT></CENTER><BR>
-Если вам понравился сайт, вы можете помочь нам материально и поддержать нас.<BR>
-Переведенные деньги пойдут на оплату хостинга и доменного имени.<BR><BR>
-<CENTER><FONT color="#FCFE54">Деньги можно перевести на банковскую карту в любом терминале на улице или в любом банкомате: </FONT></CENTER> 
-<CENTER><BR><B><FONT SIZE=+1>VISA</FONT></B>&nbsp;&nbsp;<FONT COLOR=WHITE>4890 4942 8337 5996</FONT></CENTER><BR></TD></TR>
